184f7f370b527f8bb5e151b79df778d07bbc9639
etom94 <thomaserny4@gmail.com>
2023-12-06 23:35:52 +0100

    Display bug daily weather

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 // main.dart
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart'; // Füge diese Zeile hinzu
 import 'view/HomePage.dart';
 
-void main() {
+void main() async {
+  // Initialisiere die Lokalisierung für deutsche Datums- und Zeitformate
+  await initializeDateFormatting('de_DE', null);
+
+  // Starte die Flutter-Anwendung
   runApp(const MyApp());
 }
 
--- lib/view/SiebenTageWetter.dart
+++ lib/view/SiebenTageWetter.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../model/7DayData.dart';
 
 // Funktion für den asynchronen Aufruf der Wetterdaten
-  Future<List<WeatherData>> sevenDaysData = fetchSevenDaysWeatherData();
+Future<List<WeatherData>> sevenDaysData = fetchSevenDaysWeatherData();
 
+// Funktion zur Umwandlung von Datum in Wochentag
+String convertDateToDay(String date) {
+  DateTime dateTime = DateTime.parse(date);
+  // Verwende die Methode `weekday` der Klasse DateTime, um den Wochentag zu erhalten
+  String dayOfWeek = DateFormat.E('de_DE').format(dateTime);
+  return dayOfWeek;
+}
 
-// Deine bestehende Funktion zum Erstellen des Widget
+// Deine bestehende Funktion zum Erstellen des Widgets
 Widget buildWeatherWidget() {
   return FutureBuilder<List<WeatherData>>(
     // Hier rufst du die Funktion für die Wetterdaten auf
@@ -30,53 +38,59 @@ Widget buildWeatherWidget() {
               color: Colors.white.withOpacity(0.2),
               borderRadius: BorderRadius.circular(12.0),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: weatherDataList.map((weatherData) {
-                return Container(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        weatherData.date,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          shadows: [
-                            Shadow(
-                              color: Colors.black,
-                              offset: Offset(1, 1),
-                              blurRadius: 0,
-                            ),
-                          ],
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: weatherDataList.map((weatherData) {
+                  // Hier wird das Datum in Wochentag umgewandelt
+                  String dayOfWeek = convertDateToDay(weatherData.date);
+
+                  return Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          dayOfWeek, // Hier wird der Wochentag angezeigt
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                offset: Offset(1, 1),
+                                blurRadius: 0,
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Image.network(
-                        weatherData.weatherIconUrl,
-                        // Hier kannst du die Größe oder andere Eigenschaften der Grafik anpassen
-                      ),
-                      Text(
-                        '${weatherData.minTempC} / ${weatherData.maxTempC}',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          shadows: [
-                            Shadow(
-                              color: Colors.black,
-                              offset: Offset(1, 1),
-                              blurRadius: 0,
-                            ),
-                          ],
+                        Image.network(
+                          weatherData.weatherIconUrl,
+                          // Hier kannst du die Größe oder andere Eigenschaften der Grafik anpassen
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              }).toList(),
+                        Text(
+                          '${weatherData.minTempC} / ${weatherData.maxTempC}',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                offset: Offset(1, 1),
+                                blurRadius: 0,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
             ),
           ),
         );
